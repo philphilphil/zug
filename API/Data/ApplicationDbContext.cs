24276e2da6f4ace8629a2e937abbdf9932a65666
Philipp Baum <local.tux@gmail.com>
2020-01-03 18:16:59 +0100

    switched to db

---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Switch> Switches { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=zug.db");
+    }
+}
+
---
+++ API/Migrations/20200103171548_init.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ControllerApi.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Switches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RPI_Pin1 = table.Column<int>(nullable: false),
+                    RPI_Pin2 = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Type = table.Column<int>(nullable: false),
+                    CurrentState = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Switches", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Switches");
+        }
+    }
+}
